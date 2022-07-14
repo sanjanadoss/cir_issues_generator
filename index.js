aba85208d3cf0f5b0676055cf50129e9dfde8ba0
sanjanadoss <sanjanadoss2503@gmail.com>
2022-07-15 00:39:15 +0530

    speed optimization & removed comments

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-// Submit event listener
+
 document.querySelector('#r-form').addEventListener('submit', function(e){
-    // Hide the results
+
     document.querySelector('#results').style.display = 'none';
-    // Show the loading animation
-    document.querySelector('#loading').style.display = 'block';
     document.querySelector('#cope').style.display = 'none';
-
-    setTimeout(endResult, 1000);
-
+    endResult()
     e.preventDefault();
 });
 
-// Calculate the results
+
 function endResult(){
-    // Declare UI variables
+
     var name = document.getElementById('name');
     var rollNo = document.getElementById('rollNo');
     var emailId = document.getElementById('emailId');
@@ -21,46 +17,30 @@ function endResult(){
     var issue = document.getElementById('issue');
     var copyPaste = document.getElementById('copyPaste');
 
-    if(name && rollNo && emailId && mobNo && issue){
-        let emp = "";
+    if(name.value && rollNo.value && emailId.value && mobNo.value && issue.value){
         copyPaste.value = "Name: "+ name.value+ '\r\n' + "Roll No.: "+ rollNo.value+ '\r\n' +"Email Id: "+ emailId.value+'\r\n' + "Mobile No: "+ mobNo.value+ '\r\n' +"Issue: "+ issue.value;
         document.querySelector('#results').style.display = 'block';
-        document.querySelector('#loading').style.display = 'none';
         document.querySelector('#cope').style.display = 'block';
     } else {
-        // Show an error message
         showError('Some values are left blank');
     }
 }
 
-// Show error on invalid input
+
 function showError(error){
     const copyPaste = "Yes"
-    // Hide the results and loader
+
     document.querySelector('#results').style.display = 'none';
-    document.querySelector('#loading').style.display = 'none';
 
-    // Create a div for the error message
     const errorDiv = document.createElement('div');
-
-    // Get elements
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
-
-    // Add a class to the error message div
     errorDiv.className = 'alert alert-danger';
-    
-    // Create a text nod and append to div
     errorDiv.appendChild(document.createTextNode(error));
-
-    // Insert the error message above the heading
     card.insertBefore(errorDiv, heading);
-
-    // Clear error message after 3 seconds
-    setTimeout(clearError, 4000);
+    setTimeout(clearError, 2000);
 }
 
-// Clear the error message
 function clearError(){
     document.querySelector('.alert').remove();
 }
